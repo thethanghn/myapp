15fa565a586d3af7e1a61d57d267db51c6a4704e
Nguyen The Thang <justin.ntt@gmail.com>
2016-08-09 18:09:15 +0700

    rails_apps_composer: add README files

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   root to: 'visitors#index'
   ActiveAdmin.routes(self)
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => "registrations" }
+  get 'products/:id', to: 'products#show', :as => :products
+  devise_scope :user do
+    post 'pay', to: 'registrations#pay'
+  end
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
